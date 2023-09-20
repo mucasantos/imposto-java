90c8a52bf79f911fe06941c12050500be54bb19f
Samuel Santos <mucasantos@gmail.com>
2023-09-19 21:32:32 -0300

    abstract activity

---
+++ entities/TaxPayer.java
@@ -0,0 +1,27 @@
+package entities;
+
+public abstract class TaxPayer {
+
+	private String name;
+	private Double anualIncome;
+	
+	public TaxPayer(String name, Double anualIncome) {
+		super();
+		this.name = name;
+		this.anualIncome = anualIncome;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getAnualIncome() {
+		return anualIncome;
+	}
+	public void setAnualIncome(Double anualIncome) {
+		this.anualIncome = anualIncome;
+	}
+	//Eu não estanciar! Forçar quem vai usar a minha classe, a criar esse método!
+	public abstract Double tax();
+}
